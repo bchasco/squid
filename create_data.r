7136be6845a60957419c176794cec76df2063e58
Brandon.Chasco <1508516050@mil>
2020-04-05 20:27:04 -0700

    Updated simluation testing script. Still need to simulation test the temporal and 2-D spatial model.

--- create_data.r
+++ create_data.r
@@ -41,18 +41,22 @@ Data <- list(c_i = c_i,
 Parameters <- list(fp = -0,
                    ln_lam = log(300),
                    ln_theta = log(1),
+                   tr_b = 0,
+                   tr_0 = 0,
                    eps_s = rep(0,max(Data$s_i_re)+1),
                    lnsig_eps_s = 0,
                    eps_y = rep(0,length(unique(Data$y))),
-                   frho_y = 0,
+                   frho_y = 1,
                    lnsig_eps_y = 0,
                    eps_tr = rep(0,length(unique(Data$trans_i))),
-                   frho_tr = 0,
+                   frho_tr = 0.1,
                    lnsig_eps_tr = 0,
                    eps_yt = matrix(0,length(unique(Data$trans_i)),length(unique(Data$y))),
                    frho_yt_1 = 0,
-                   frho_yt_2 = 0,
-                   lnsig_eps_yt = 0)
+                   frho_yt_2 = 0.1,
+                   lnsig_eps_yt = 0,
+                   fdecor_tr=0,
+                   fdecor_yt_tr=0)
 
 
 
@@ -87,27 +91,55 @@ sim_s <- 1
 sim_y <- 1
 sim_yt <- 1
 
+nsim <-100
+
+testCor = 4
 runSim <- TRUE
 
 myMap <- list()
-
+estNames <- list()
 if(!est_tr){
   myMap <- append(myMap
                   ,map_tr)
+  tmpNames <- list(sig_tr=NA,
+                   rho_tr=NA)
+  estNames <- append(estNames,
+                  tmpNames)
 }
 if(!est_s){
   myMap <- append(myMap
                   ,map_s)
+  tmpNames <- list(sig_s=NA)
+  estNames <- append(estNames,
+                  tmpNames)
 }
 if(!est_y){
   myMap <- append(myMap
                   ,map_y)
+  tmpNames <- list(sig_y=NA,
+                   rho_y=NA)
+  estNames <- append(estNames,
+                  tmpNames)
+  
 }
 if(!est_yt){
   myMap <- append(myMap
                   ,map_yt)
+  tmpNames <- list(sig_yt=NA,
+                   rho_yt_1=NA,
+                   rho_yt_2=NA)
+  estNames <- append(estNames,
+                  tmpNames)
+  
 }
 
+myMap <- append(myMap,
+                list(tr_0=as.factor(NA),
+                     fdecor_tr = as.factor(NA),
+                     fdecor_yt_tr = as.factor(NA))
+                )
+# myMap <- append(myMap,
+#                 list(fp=as.factor(NA)))
 # myMap <- append(myMap
 #                   ,list(ln_lam=as.factor(NA),
 #                   ln_theta=as.factor(NA)))
@@ -119,7 +151,8 @@ Data <- append(Data,
                list(est_tr = est_tr,
                est_y = est_y,
                est_s = est_s,
-               est_yt = est_yt))
+               est_yt = est_yt,
+               testCor = testCor))
 # Make object
 Obj = MakeADFun(data=Data,
                 parameters=Parameters,
@@ -135,31 +168,72 @@ Obj = MakeADFun(data=Data,
 # Check_Identifiable(Obj)
 
 out <- nlminb(Obj$par,Obj$fn,Obj$gr)
-rep <- Obj$report()
+out_rep <- Obj$report()
+parNames <- c("sig_s",
+              "sig_tr",
+              "sig_y",
+              "sig_yt",
+              "rho_tr",
+              "rho_y",
+              "rho_yt_1",
+              "rho_yt_2",
+              "lam",
+              "p",
+              "theta")
 
+estPars <- c(out_rep$sig_eps_s,
+                 out_rep$sig_eps_tr,
+                 out_rep$sig_eps_y,
+                 out_rep$sig_eps_yt,
+             out$par["frho_tr"],
+                 out_rep$rho_y,
+                 out_rep$rho_yt_1,
+             out$par["frho_yt_2"],
+                 exp(out_rep$ln_lam),
+                 out_rep$p,
+                 out_rep$theta)
+
+estParNames <- parNames[!(parNames%in%names(estNames))]
 
 i <- 1
 if(runSim){
   myMap <- list()
+  simNames <- list()
   
   if(!sim_tr){
     myMap <- append(myMap
                     ,map_tr)
+    tmpNames <- list(sig_tr=NA,
+                     rho_tr=NA)
+    simNames <- append(simNames,
+                       tmpNames)
   }
   if(!sim_s){
     myMap <- append(myMap
                     ,map_s)
+    tmpNames <- list(sig_s=NA)
+    simNames <- append(simNames,
+                       tmpNames)
   }
   if(!sim_y){
     myMap <- append(myMap
                     ,map_y)
+    tmpNames <- list(sig_y=NA,
+                     rho_y=NA)
+    simNames <- append(simNames,
+                       tmpNames)
   }
   if(!sim_yt){
     myMap <- append(myMap
                     ,map_yt)
+    tmpNames <- list(sig_yt=NA,
+                     rho_yt_1=NA,
+                     rho_yt_2=NA)
+    simNames <- append(simNames,
+                       tmpNames)
   }
   
-  simout <- replicate(500,{
+  simout <- replicate(nsim,{
     simData <- Obj$simulate(par = Obj$env$last.par.best, complete = TRUE)
     simData$est_s <- sim_s
     simData$est_y <- sim_y
@@ -174,28 +248,67 @@ if(runSim){
                                "eps_yt")#Spatio-temporal
                       ,
                       hessian=TRUE,
-                      silent = TRUE,
+                      silent = FALSE,
                       map=myMap,
                       DLL="squid2")
     print(i)
     i <- i+1
-    sim_i <- nlminb(obj2$par,obj2$fn,obj2$gr)$par
-    # rep <- obj2$report()
-    sim_i})
+    sim_i <- nlminb(obj2$par,obj2$fn,obj2$gr)
+    rep <- obj2$report()
+    par(mfrow=c(3,2))
+    plot(simData$eps_tr,
+         col="darkgrey",
+         cex=1.2,
+         pch=16,
+         ylim=c(min(simData$eps_tr,rep$eps_tr),max(simData$eps_tr,rep$eps_tr)))
+    lines(rep$eps_tr)
+    plot(simData$eps_y, 
+         col="darkgrey",
+         cex=1.2,
+         pch=16,
+         ylim=c(min(simData$eps_y,rep$eps_y),max(simData$eps_y,rep$eps_y)))
+    lines(rep$eps_y)
+    hist(simData$eps_s)
+    hist(rep$eps_s, col="red")
+    image(rep$eps_yt)
+    image(simData$eps_yt)
+    
+    c(rep$sig_eps_s,
+      rep$sig_eps_tr,
+      rep$sig_eps_y,
+      rep$sig_eps_yt,
+      sim_i$par["frho_tr"],
+      rep$rho_y,
+      rep$rho_yt_1,
+      sim_i$par["frho_yt_2"],
+      exp(rep$ln_lam),
+      rep$p,
+      rep$theta)})
+
+  row.names(simout) <- parNames
   
   simMelt <- melt(t(simout),measure.vars = colnames(simout),variable.name="pars")
+  simMelt <- simMelt[!(simMelt$Var2%in%names(simNames)),]
+  sim_par <- unique(simMelt$Var2)
+  for(ii in sim_par){
+    dat_q <- quantile(simMelt$value[simMelt$Var2==ii], probs=c(0.025,0.975))
+    simMelt <- simMelt[!(simMelt$Var2==ii & (simMelt$value<dat_q[1] | simMelt$value>=dat_q[2])),]
+  }
   library(ggplot2)
   
-  myLines <- data.frame(Var2=names(Obj$env$last.par.best),
-                        value=Obj$env$last.par.best)
-  myLines <- myLines[myLines$Var2%in%rownames(simout),]
+  myLines <- data.frame(Var2=parNames,
+                        value=estPars)
+  myLines <- myLines[!(myLines$Var2%in%names(estNames)) |
+                       !(myLines$Var2%in%names(simNames)),]
   
-  p <- ggplot(simMelt, aes(value)) +
+  p <- ggplot(simMelt, aes(factor(Var2),value)) +
     facet_wrap(~Var2, scales = "free") +
-    geom_histogram()+
-    geom_vline(aes(xintercept=value), colour="blue",myLines)
+    geom_violin(aes(factor(Var2)))+
+    geom_hline(aes(yintercept=value), colour="blue",myLines)
   
   print(p)
 }
 
-save(simout,file="simout.out")
+save(simout,
+     file=paste("simout",paste(est_s,est_tr,est_y,est_yt,sep="",collapse = ""),
+                ".out",sep=""))
--- squid2.cpp
+++ squid2.cpp
@@ -21,11 +21,14 @@ Type objective_function<Type>::operator() ()
   DATA_INTEGER(est_y);
   DATA_INTEGER(est_s);
   DATA_INTEGER(est_yt);
+  DATA_INTEGER(testCor);
   
   // Fixed effects
   PARAMETER(fp);   //proportin with catches
   PARAMETER(ln_lam);
   PARAMETER(ln_theta);
+  PARAMETER(tr_b);
+  PARAMETER(tr_0);
   PARAMETER_VECTOR(eps_s);
   PARAMETER(lnsig_eps_s);
   PARAMETER_VECTOR(eps_y);
@@ -34,7 +37,9 @@ Type objective_function<Type>::operator() ()
   PARAMETER_VECTOR(eps_tr);
   PARAMETER(frho_tr);
   PARAMETER(lnsig_eps_tr);
-
+  PARAMETER(fdecor_tr);
+  PARAMETER(fdecor_yt_tr);
+  
   PARAMETER_ARRAY(eps_yt);
   PARAMETER(frho_yt_1);
   PARAMETER(frho_yt_2);
@@ -47,11 +52,13 @@ Type objective_function<Type>::operator() ()
   int n_i = c_i.size();
   
   // Likelihood contribution from observations
+  Type decor_tr = exp(fdecor_tr);
+  Type decor_yt_tr = exp(fdecor_yt_tr);
   Type p = exp(fp)/(1+exp(fp));
   Type rho_y = 1/(1+exp(-frho_y)); //atan(frho_y)*3.154/2.; //
-  Type rho_tr = exp(frho_tr);//1/(1+exp(-frho_tr)); //atan(frho_tr)*3.154/2.;//Type(2)/(Type(1) + exp(-Type(2) * frho_y)) - Type(1);//
-  Type rho_yt_1 = atan(frho_yt_1)*3.154/2.;//1/(1+exp(-frho_yt_1)); //Type(2)/(Type(1) + exp(-Type(2) * frho_yt_1)) - Type(1);//
-  Type rho_yt_2 = exp(frho_yt_2);//1/(1+exp(-frho_yt_2));//atan(frho_yt_2)*3.154/2.;//
+  Type rho_tr = 1/(1+exp(-frho_tr));//1/(1+exp(-frho_tr)); //atan(frho_tr)*3.154/2.;//Type(2)/(Type(1) + exp(-Type(2) * frho_y)) - Type(1);//
+  Type rho_yt_1 = atan(frho_yt_1)*2/3.154;//1/(1+exp(-frho_yt_1)); //Type(2)/(Type(1) + exp(-Type(2) * frho_yt_1)) - Type(1);//
+  Type rho_yt_2 = 1/(1+exp(-frho_yt_2));//atan(frho_yt_2)*2/3.154;//1/(1+exp(frho_yt_2));//
   Type theta = exp(ln_theta);
   Type sig_eps_s = exp(lnsig_eps_s);
   Type sig_eps_y = exp(lnsig_eps_y);
@@ -74,20 +81,31 @@ Type objective_function<Type>::operator() ()
     for(int i=0;i<nt;i++){
       for(int j=0;j<nt;j++){
         if(j!=i){
-          Sigma_yt(i,j) = exp(-1.*rho_yt_2*abs(transDist(i)-transDist(j)));
+          Type d = transDist(i)-transDist(j);
+          if(testCor==1){
+            Sigma_yt(i,j) = 1/(1+exp(-frho_yt_2));
+          }
+          if(testCor==2){
+            Sigma_yt(i,j) = exp(-(d)/exp(frho_yt_2));
+          }
+          if(testCor==3){
+            Sigma_yt(i,j) = exp(-(d*d)/(frho_yt_2*frho_yt_2));
+          }
+          if(testCor==4){
+            Sigma_yt(i,j) = pow(rho_yt_2, abs(transDist(i)-transDist(j))/0.1);
+          }
+          if(testCor==5){
+            Sigma_yt(i,j) = rho_yt_2 * exp(-1.*abs(transDist(i)-transDist(j))/0.1);//
+          }
         }
         if(j==i){
           Sigma_yt(i,j) = 1.;
         }
       }
     }
-    // jnll += AR1(rho_yt_2,AR1(rho_yt_1))(eps_yt);
     jnll += SEPARABLE(AR1(rho_yt_1),MVNORM(Sigma_yt))(eps_yt);
-    
-    // for(int i=0;i<eps_tr.size();i++){
-    //   jnll -= dnorm(eps_yt(i,0),Type(0.), Type(1.0),true);
-    // }
   }
+  
   if(est_s){
     jnll -= dnorm(eps_s,Type(0.),sig_eps_s,true).sum();
   }
@@ -97,7 +115,22 @@ Type objective_function<Type>::operator() ()
   for(int i=0;i<nt;i++){
     for(int j=0;j<nt;j++){
       if(j!=i){
-        Sigma(i,j) = exp(-1.*rho_tr*abs(transDist(i)-transDist(j)));
+        Type d = transDist(i)-transDist(j);
+        if(testCor==1){
+          Sigma(i,j) = 1/(1+exp(-frho_tr));
+        }
+        if(testCor==2){
+          Sigma(i,j) = exp(-(d)/exp(frho_tr));
+        }
+        if(testCor==3){
+          Sigma(i,j) = exp(-(d*d)/(frho_tr*frho_tr));
+        }
+        if(testCor==4){
+          Sigma(i,j) = pow(rho_tr, abs(transDist(i)-transDist(j))/0.1);
+        }
+        if(testCor==5){
+          Sigma(i,j) = rho_tr * exp(-1.*abs(transDist(i)-transDist(j))/0.1);//
+        }
       }
       if(j==i){
         Sigma(i,j) = 1.;
@@ -106,11 +139,7 @@ Type objective_function<Type>::operator() ()
   }
       
   if(est_tr){
-    // jnll += AR1(rho_tr)(eps_tr);
     jnll += MVNORM(Sigma)(eps_tr);
-    // for(int i=1;i<nt;i++){
-    //   jnll -= dnorm(eps_tr(i),pow(rho_tr,abs(transDist(i)-transDist(i-1))/0.1)*eps_tr(i-1),1.,true);
-    // }
   }
 
     
@@ -124,7 +153,7 @@ Type objective_function<Type>::operator() ()
       log_chat_i(i) = ln_lam + 
         eps_s(s_i_re(s_i(i))) + //Station effect
         eps_y(y_i(i)) * sig_eps_y +//Year effect
-        eps_tr(trans_i(i)) * sig_eps_tr + //Transect/latitude effect
+        trans_i(i)*tr_b + eps_tr(trans_i(i)) * sig_eps_tr + //Transect/latitude effect
         eps_yt(trans_i(i),y_i(i))*sig_eps_yt; //Interaction between transect and latitude
       
       jnll -= log(p);
@@ -147,7 +176,6 @@ Type objective_function<Type>::operator() ()
     }
     if(est_yt){
       AR1(rho_yt_1,MVNORM(Sigma_yt)).simulate(eps_yt);
-      // AR1(rho_yt_2,AR1(rho_yt_1)).simulate(eps_yt);
     }else{
       eps_yt.fill(0.);
     }
@@ -162,11 +190,11 @@ Type objective_function<Type>::operator() ()
     for (int i=0; i<n_i; i++){
       if(c_i(i)>0.){
         log_chat_i(i) = ln_lam + 
-        eps_s(s_i_re(s_i(i))) + 
-        eps_y(y_i(i)) * sig_eps_y +
-        eps_tr(trans_i(i)) * sig_eps_tr +
-        eps_yt(trans_i(i),y_i(i)) * sig_eps_yt;
-        c_i(i) = rgamma(pow(theta,-2), pow(theta,2)*exp(log_chat_i(i)));  // Simulate response
+          eps_s(s_i_re(s_i(i))) + 
+          eps_y(y_i(i)) * sig_eps_y +
+          tr_b*trans_i(i) + eps_tr(trans_i(i)) * sig_eps_tr +
+          eps_yt(trans_i(i),y_i(i)) * sig_eps_yt;
+        c_i(i) = rgamma(a, exp(log_chat_i(i))/a);  // Simulate response
       }
     }
     REPORT(c_i);          // Report the simulation
@@ -203,5 +231,7 @@ Type objective_function<Type>::operator() ()
   REPORT(ln_lam);
   REPORT(Sigma);  
   REPORT(a);
+  REPORT(tr_0);
+  REPORT(tr_b);
   return jnll;
 }
